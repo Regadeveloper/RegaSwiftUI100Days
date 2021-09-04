8fecf9861dd87cbb52899e1abfe978a6162f4d72
Raúl <raulregadera@gmail.com>
2021-09-04 19:20:59 +0200

    added missing files

---
+++ Projects/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Raul on 20/5/21.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
