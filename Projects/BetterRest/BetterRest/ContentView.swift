b09250a9fde6c69afb231bd21f188985125571a2
Raúl <raulregadera@gmail.com>
2021-09-05 12:51:44 +0200

    modified the layout

--- Projects/BetterRest/BetterRest/ContentView.swift
+++ Projects/BetterRest/BetterRest/ContentView.swift
@@ -17,18 +17,52 @@ struct ContentView: View {
             VStack{
                 Text("How many hours do you want to sleep?")
                     .font(.headline)
+                    .padding(.top)
+                    .padding(.horizontal)
+                    .frame(maxWidth: .infinity, alignment: .leading)
                 Stepper(value: $sleepAmount, in: 3...14, step: 0.25) {
                     Text ("\(sleepAmount, specifier: "%g") hours")
                 }
                 .padding()
+
+                Text ("At what time do you want to wake up?")
+                    .font(.headline)
+                    .padding(.top)
+                    .padding(.horizontal)
+                    .frame(maxWidth: .infinity, alignment: .leading)
                 DatePicker ("Please enter a time:", selection: $wakeUp, in: Date()...,
                             displayedComponents: .hourAndMinute)
                     .datePickerStyle(WheelDatePickerStyle())
                     .labelsHidden()
                     .padding()
+
+                Text ("Daily coffee intake:")
+                    .font(.headline)
+                    .padding(.top)
+                    .padding(.horizontal)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                Stepper(value: $coffeeAmount, in: 0...12, step: 1 ) {
+                    if coffeeAmount == 1 {
+                        Text ("1 cup")
+                    }
+                    else {
+                        Text ("\(coffeeAmount) cups")
+                    }
+                }
+                .padding()
+                Spacer()
             }
             .padding()
+            .navigationBarTitle("BetterRest")
+            .navigationBarItems(
+            trailing:
+                Button(action: calculateBedtime){
+                    Text("Calculate")
+                }
+            )
         }
+    }
+    func calculateBedtime(){
 
     }
 }
@@ -39,6 +73,8 @@ func buildFormater() -> String {
     let dateString = formatter.string(from: Date())
     return dateString
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
